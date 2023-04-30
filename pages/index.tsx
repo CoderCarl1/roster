9ce0350996fbc139907a5f82b19bb0b50fa7b386
carl davidson <carl.davidson@hotmail.com>
2023-04-30 15:48:56 +1000

    add a 7 day roster

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,8 @@ import { useState, useEffect } from "react";
 import type { Tperson, } from "../types";
 import Timetable from "../components/Timetable";
 
-export default function Home() {
+const useUtils = () => {
   const [customers, setCustomers] = useState<Tperson[]>([]);
-
 
   useEffect(() => {
     async function fetchCustomers() {
@@ -16,10 +15,35 @@ export default function Home() {
     fetchCustomers();
   }, []);
 
+  const today = new Date();
+  const week = [];
+
+  // generate array of 7 days starting from today
+  for (let i = 0; i < 7; i++) {
+    const date = new Date(today);
+    date.setDate(today.getDate() + i);
+    week.push(date);
+  }
+
+
+  return {customers, today, week}
+}
+
+export default function Home() {
+
+  const {customers, today, week} = useUtils();
+
   return (
     <div>
       <h1>Appointments</h1>
-      <Timetable customers={customers} date={new Date()}/>
+      {/* <Timetable customers={customers} date={new Date()}/> */}
+      {week.map((date, index) => (
+        <div key={index}>
+          <h2>{date.toLocaleDateString("en-GB", { weekday: "long", day: "numeric", month: "long" })}</h2>
+          <Timetable customers={customers} date={date} />
+        </div>
+      ))}
+
     </div>
   );
 }
