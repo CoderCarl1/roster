427ef3bf7adb457d4b0df3367702b9023cd106dc
carl davidson <carl.davidson@hotmail.com>
2023-11-14 11:43:04 +1000

    updated script file for seeding

--- prisma/script.ts
+++ prisma/script.ts
@@ -1,51 +1,60 @@
-import { Address, Appointment, Customer } from "@prisma/client";
-
-import { prisma } from "~/db.server";
+import { PrismaClient } from "@prisma/client";
+import { TAddress_No_ID, TAppointment_No_ID, TCustomer_No_ID } from "@types";
 import { log } from "~/functions/helpers/functions";
 import { findAddresses_byCustomer } from "~/models/address";
 import { createAppointment } from "~/models/appointments";
 import { createCustomer } from "~/models/customer";
 
+import { singleton } from "~/singleton.server";
 import {
   customers,
   addresses,
   appointments,
 } from "../app/lib/placeholder-data";
 
-export type TCustomer_No_ID = Omit<Customer, "id" | "createdAt" | "updatedAt">;
-export type TAddress_No_ID = Omit<
-  Address,
-  "id" | "customerId" | "createdAt" | "updatedAt"
->;
-export type TAppointment_No_ID = Omit<
-  Appointment,
-  "id" | "customerId" | "addressId" | "createdAt" | "updatedAt"
->;
+const seedingFlagIndex = process.argv.indexOf("--seeding") + 1;
+if (seedingFlagIndex !== -1) {
+  process.env.SEEDING = process.argv[seedingFlagIndex] || "false";
+}
+
+const prisma = singleton(
+  "prismaScriptFile",
+  (seeding: string | undefined) =>
+    new PrismaClient({
+      log: seeding === "true" ? [] : ["query", "info", "warn", "error"],
+    }),
+  process.env.SEEDING,
+);
 
 async function main() {
-  seed();
+  if (process.env.SEEDING === "true") {
+    await seed();
+  }
 }
 
 main()
   .catch((error) => {
-    log("red", "DB Seed error", error);
+    if (process.env.SEEDING === "true") {
+      log("red", "SEEDING ERROR", error);
+    } else {
+      log("red", "Error", error);
+    }
   })
   .finally(async () => {
-    log("blue", "disconnecting from Prisma client");
     await prisma.$disconnect().then(() => log("yellow", "disconnected"));
   });
 
 async function seed() {
   log("magenta", "==================");
-  log("magenta", "cleaning DB before Seeding");
+  log("magenta", "Cleaning DB before Seeding");
   await prisma.customer
     .deleteMany({ where: { contact: { endsWith: "example.com" } } })
-    .then(() => log("magenta", "DELETED all records"))
+    .then(() => log("magenta", "DELETED all previous example records"))
     .catch((err) => {
       log("red", "error deleting customers", err);
     });
 
-  log("magenta", "creating customers");
+  log("magenta", "CREATING Customers");
 
   for (const customer of customers) {
     const addressArray = addresses.filter(
@@ -64,25 +73,33 @@ async function seed() {
       Object.assign(customerData, { addresses: { create: addressData } });
     }
 
-    const createdCustomer = await createCustomer(customerData);
+    const createdCustomer = await prisma.customer.create({
+      data: customerData,
+    });
     if (createdCustomer !== null) {
-      const customerAddresses = await findAddresses_byCustomer(
-        createdCustomer.id,
-      );
+      const customerAddresses = await prisma.address.findMany({
+        include: { Customer: true },
+        where: {
+          customerId: createdCustomer.id,
+        },
+      });
+
       for (const appointment of customerAppointments) {
         const appointmentData: TAppointment_No_ID =
           prepareDataForSeeding(appointment);
-
-        await createAppointment({
-          ...appointmentData,
-          customerId: createdCustomer.id,
-          addressId: customerAddresses[0].id,
+        await prisma.appointment.create({
+          data: {
+            ...appointmentData,
+            customerId: createdCustomer.id,
+            addressId: customerAddresses[0].id,
+          },
         });
       }
     }
   }
   log("green", `Database has been seeded. 🌱`);
   log("magenta", "==================");
+  return null;
 }
 
 function prepareDataForSeeding<T extends object>(obj: T) {
