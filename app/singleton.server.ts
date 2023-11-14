3dff31d56fbcab9f5eeb288234e06bbf5d3694f6
carl davidson <carl.davidson@hotmail.com>
2023-11-14 11:42:41 +1000

    updated singleton server file

--- app/singleton.server.ts
+++ app/singleton.server.ts
@@ -1,9 +1,10 @@
-export const singleton = <Value>(
+export const singleton = <Value, Arg>(
   name: string,
-  valueFactory: () => Value,
+  valueFactory: (optionalArg?: Arg) => Value,
+  optionalArg?: Arg,
 ): Value => {
   const g = global as unknown as { __singletons: Record<string, unknown> };
   g.__singletons ??= {};
-  g.__singletons[name] ??= valueFactory();
+  g.__singletons[name] ??= valueFactory(optionalArg);
   return g.__singletons[name] as Value;
 };
