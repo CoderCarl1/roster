dfeff7345dab7c204021cc3895b24039af8a649e
carl davidson <carl.davidson@hotmail.com>
2023-11-14 14:35:26 +1000

    create mock prisma as per prisma docs

--- jest.config.ts
+++ jest.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "jest";
+import type {Config} from 'jest';
 
 const config: Config = {
   // All imported modules in your tests should be mocked automatically
@@ -6,104 +6,33 @@ const config: Config = {
 
   // Stop running tests after `n` failures
   bail: 1,
-
-  // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "/tmp/jest_rs",
-
-  // Automatically clear mock calls, instances, contexts and results before every test
   clearMocks: true,
-
-  // Indicates whether the coverage information should be collected while executing the test
   collectCoverage: true,
-
-  // An array of glob patterns indicating a set of files for which coverage information should be collected
   collectCoverageFrom: [
-    "**/*.{js,jsx,ts,tsx}",
-    "!**/*.d.ts",
-    "!**/node_modules/**",
+    '**/*.{js,jsx,ts,tsx}',
+    '!**/*.d.ts',
+    '!**/node_modules/**',
   ],
-
-  // The directory where Jest should output its coverage files
   coverageDirectory: "coverage",
-
-  // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
-
   // The default configuration for fake timers
   // fakeTimers: {
   //   "enableGlobally": false
   // },
-
-  // A path to a module which exports an async function that is triggered once before all test suites
-  // globalSetup: undefined,
-
-  // A path to a module which exports an async function that is triggered once after all test suites
-  // globalTeardown: undefined,
-
-  // A set of global variables that need to be available in all test environments
-  // globals: {},
-
-  // The maximum amount of workers used to run your tests. Can be specified as % or a number. E.g. maxWorkers: 10% will use 10% of your CPU amount + 1 as the maximum worker number. maxWorkers: 2 will use a maximum of 2 workers.
-  // maxWorkers: "50%",
-
-  // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
-    "~/(.*)": "../<rootDir>/app/$1",
+    '~/(.*)': '../<rootDir>/app/$1',
   },
-
-  // A preset that is used as a base for Jest's configuration
-  // preset: undefined,
-
-  // Automatically reset mock state before every test
   resetMocks: true,
-
-  // Reset the module registry before running each individual test
-  // resetModules: false,
-
-  // Automatically restore mock state and implementation before every test
   restoreMocks: false,
-
-  // The root directory that Jest should scan for tests and modules within
-  // rootDir: undefined,
-
-  // The paths to modules that run some code to configure or set up the testing environment before each test
-  // setupFiles: [],
-
-  // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
-
-  // The number of seconds after which a test is considered as slow and reported as such in the results.
+  setupFilesAfterEnv: ['<rootDir>/../tests/mocks/prismaclient.ts'],
   slowTestThreshold: 5,
-
-  // The test environment that will be used for testing
   testEnvironment: "jest-environment-node",
-
-  // Options that will be passed to the testEnvironment
-  // testEnvironmentOptions: {},
-
-  // Adds a location field to test results
-  // testLocationInResults: false,
-
-  // The glob patterns Jest uses to detect test files
-  testMatch: ["<rootDir>/tests/**/*.{js,jsx,ts,tsx}"],
-
-  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testMatch: ['<rootDir>/tests/**/*.{js,jsx,ts,tsx}'],
   testPathIgnorePatterns: [
-    "<rootDir>/node_modules/",
-    "<rootDir>/.cache/",
-    "<rootDir>/build/",
+    '<rootDir>/node_modules/',
+    '<rootDir>/.cache/',
+    '<rootDir>/build/',
   ],
-
-  // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ["/node_modules/"],
-
-  // Indicates whether each individual test should be reported during the run
-  // verbose: undefined,
+  transformIgnorePatterns: ['/node_modules/'],
 };
 
 export default config;
