5d2d08a687067a607ecf1e9c67a83f8936d44839
carl davidson <carl.davidson@hotmail.com>
2023-11-14 11:41:45 +1000

    add types used for seeding to types file

--- types.d.ts
+++ types.d.ts
@@ -12,3 +12,14 @@ export type TAddress_data_for_creation = Omit<
   Address,
   "id" | "createdAt" | "updatedAt"
 >;
+
+// Types used for Seeding
+export type TCustomer_No_ID = Omit<Customer, "id" | "createdAt" | "updatedAt">;
+export type TAddress_No_ID = Omit<
+  Address,
+  "id" | "customerId" | "createdAt" | "updatedAt"
+>;
+export type TAppointment_No_ID = Omit<
+  Appointment,
+  "id" | "customerId" | "addressId" | "createdAt" | "updatedAt"
+>;
