21adcfaa8daedd398d20880cd2d2dcdadccb63ef
carl davidson <carl.davidson@hotmail.com>
2023-11-16 15:55:48 +1000

    add additional type definitions

--- types.d.ts
+++ types.d.ts
@@ -4,13 +4,38 @@ export type TCustomer_data_for_creation = Omit<
     Customer,
     'id' | 'suspended' | 'suspendedAt' | 'createdAt' | 'updatedAt'
 >;
+
+export type TCustomerDataWithAddresses_for_creation =
+    TCustomer_data_for_creation & {
+        addresses: TAddress_data_for_creation[] | Address[];
+    };
+export type TCustomerDataWithAppointmentsAndAddresses_for_creation =
+    TCustomer_data_for_creation & {
+        addresses: TAddress_data_for_creation[] | Address[];
+        appointments: TAppointment_data_for_creation[] | Appointment[];
+    };
+export type TCustomer_Appointments_Addresses_for_creation =
+    | TCustomer_data_for_creation
+    | TCustomerDataWithAddresses_for_creation
+    | TCustomerDataWithAppointmentsAndAddresses_for_creation;
+
+export type TCustomer_Appointments_Addresses =
+    | Customer
+    | (Customer & {
+          addresses: Address[];
+      })
+    | (Customer & {
+          addresses: Address[];
+          appointments: Appointment[];
+      });
+
 export type TAppointment_data_for_creation = Omit<
     Appointment,
     'id' | 'customerId' | 'addressId' | 'completed' | 'createdAt' | 'updatedAt'
 >;
 export type TAddress_data_for_creation = Omit<
     Address,
-    'id' | 'customerId' | 'createdAt' | 'updatedAt'
+    'id' | 'customerId' | 'archived' | 'createdAt' | 'updatedAt'
 >;
 
 // Types used for Seeding
@@ -21,5 +46,5 @@ export type TAddress_No_ID = Omit<
 >;
 export type TAppointment_No_ID = Omit<
     Appointment,
-    'id' | 'customerId' | 'addressId' | 'createdAt' | 'updatedAt'
+    'id' | 'createdAt' | 'updatedAt'
 >;
