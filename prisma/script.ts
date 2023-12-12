6b156c68c5ec7b61992b96d08fe467a800a0e5c9
carl davidson <carl.davidson@hotmail.com>
2023-12-12 14:35:53 +1000

    updated seed and fake data generators

--- prisma/script.ts
+++ prisma/script.ts
@@ -1,109 +1,241 @@
 import { PrismaClient } from '@prisma/client'
-import { TAddress_No_ID, TAppointment_No_ID, TCustomer_No_ID } from '@types'
+import { TAddress_No_ID, TAppointment_No_ID, TCustomer, TCustomer_No_ID } from '@types'
+import { AddressOperationError, AppointmentOperationError, CustomerOperationError } from '~/functions/errors'
 import { log } from '~/functions/helpers/functions'
-import { customer_delete_many } from '~/models/customer.server'
+import { appointment_create } from '~/models/appointment.server'
+import { customer_create, customer_delete_many } from '~/models/customer.server'
 import { singleton } from '~/singleton.server'
-import { customers, addresses, appointments } from '../app/lib/placeholder-data'
+import { firstNames, lastNames, addressLine2Types, streetNames, streetTypes, suburbs } from '../app/lib/placeholder-data'
 
 const seedingFlagIndex = process.argv.indexOf('--seeding') + 1
 if (seedingFlagIndex !== -1) {
-    process.env.SEEDING = process.argv[seedingFlagIndex] || 'false'
+    process.env.SEEDING = process.argv[ seedingFlagIndex ] || 'false'
 }
 
 const prisma = singleton(
     'prismaScriptFile',
     (seeding: string | undefined) =>
         new PrismaClient({
-            log: seeding === 'true' ? [] : ['query', 'info', 'warn', 'error'],
+            log: seeding === 'true' ? [] : [ 'query', 'info', 'warn', 'error' ],
         }),
     process.env.SEEDING
 )
 
-async function main() {
+
+
+try {
+    main();
+} catch (err) {
     if (process.env.SEEDING === 'true') {
-        await seed()
+        log('red', 'SEEDING ERROR', err)
+    } else {
+        log('red', 'Error', err)
+    }
+} finally {
+    async () => {
+        await prisma.$disconnect().then(() => log('yellow', 'disconnected'))
     }
 }
 
-main()
-    .catch((error) => {
-        if (process.env.SEEDING === 'true') {
-            log('red', 'SEEDING ERROR', error)
-        } else {
-            log('red', 'Error', error)
-        }
-    })
-    .finally(async () => {
-        await prisma.$disconnect().then(() => log('yellow', 'disconnected'))
-    })
+async function main() {
+    await deleteAll();
+    if (process.env.SEEDING === 'true') {
+        await seed();
+    }
+}
+
+async function deleteAll() {
+    try {
+        log('magenta', '================== \n - Cleaning DB before Seeding ');
+        await Promise.all([ customer_delete_many('example.com'), customer_delete_many('test.com') ]);
+        log('red', 'Deleted');
+        log('magenta', '==================');
+        return Promise.resolve();
+    } catch (err) {
+        console.log("error occured deleting records");
+    }
+}
 
 async function seed() {
-    log('magenta', '==================')
-    log('magenta', 'Cleaning DB before Seeding')
-    customer_delete_many('example.com')
-    log('magenta', 'CREATING Customers')
+    log('magenta', '================== \n - CREATING Customers - ')
 
-    for (const customer of customers) {
-        const addressArray = addresses.filter(
-            (address) => address.customerId === customer.id
-        )
-        const customerAppointments = appointments.filter(
-            (appointment) => appointment.customerId === customer.id
-        )
+    for (let i = 0; i < 30; i++) {
+        const result = await createMockCustomer();
+        const { id, addresses } = result as TCustomer;
+        if (addresses && addresses.length) {
+            for (let i = 0; i < addresses.length; i++) {
+                const appointmentData = randomAppointment();
+                const appointment = await appointment_create(id, addresses[ i ].id, appointmentData);
 
-        const customerData: TCustomer_No_ID = prepareDataForSeeding(customer)
+                if (appointment && !(appointment instanceof AppointmentOperationError) && appointment.frequency) {
+                    const numberOfAppointments = appointmentsPerYear(appointment.frequency);
+                    let currentNumberOfAppointmentsBooked = 1;
+                    const startDate = new Date(appointmentData.start);
+                    const endDate = new Date(appointmentData.end);
 
-        if (addressArray) {
-            const addressData: TAddress_No_ID[] = addressArray.map((address) =>
-                prepareDataForSeeding(address)
-            )
-            Object.assign(customerData, { addresses: { create: addressData } })
-        }
+                    for (currentNumberOfAppointmentsBooked; currentNumberOfAppointmentsBooked < numberOfAppointments; currentNumberOfAppointmentsBooked++) {
+                        const newStartDateTime = addWeeks(startDate, currentNumberOfAppointmentsBooked);
+                        const newEndDateTime = addWeeks(endDate, currentNumberOfAppointmentsBooked);
+                        const updatedAppointmentData = { ...appointmentData, start: newStartDateTime, end: newEndDateTime }
 
-        const createdCustomer = await prisma.customer.create({
-            data: customerData,
-        })
-        if (createdCustomer !== null) {
-            const customerAddresses = await prisma.address.findMany({
-                include: { Customer: true },
-                where: {
-                    customerId: createdCustomer.id,
-                },
-            })
-
-            for (const appointment of customerAppointments) {
-                const appointmentData: TAppointment_No_ID =
-                    prepareDataForSeeding(appointment)
-                await prisma.appointment.create({
-                    data: {
-                        ...appointmentData,
-                        customerId: createdCustomer.id,
-                        addressId: customerAddresses[0].id,
-                    },
-                })
+                        await appointment_create(id, addresses[ i ].id, updatedAppointmentData);
+                    }
+                }
             }
         }
     }
     log('green', `Database has been seeded. 🌱`)
     log('magenta', '==================')
-    return null
+    return Promise.resolve();
 }
 
-function prepareDataForSeeding<T extends object>(obj: T) {
-    if ('id' in obj) {
-        delete obj['id']
+function addWeeks(date: Date, weeks: number) {
+    const dateCopy = new Date(date);
+    dateCopy.setDate(date.getDate() + 7 * weeks);
+    return dateCopy.toISOString();
+}
+function appointmentsPerYear(appointmentFrequency: number) {
+    const daysInYear = 365;
+    return Math.floor(daysInYear / appointmentFrequency);
+}
+
+function randomNumber(ceiling: number) {
+    return Math.floor(Math.random() * ceiling) + 1;
+}
+
+function randomDateInPast(days: number) {
+    const pastDate = new Date();
+    pastDate.setDate(pastDate.getDate() - randomNumber(days));
+    return pastDate;
+}
+
+function randomDateInFuture(days: number) {
+    const futureDate = new Date();
+    futureDate.setDate(futureDate.getDate() + randomNumber(days));
+    return futureDate;
+}
+
+function randomDateBetween(startDate: Date, endDate: Date) {
+    const startTimestamp = startDate.getTime();
+    const endTimestamp = endDate.getTime();
+    const randomTimestamp = startTimestamp + Math.random() * (endTimestamp - startTimestamp);
+    return new Date(randomTimestamp);
+}
+
+
+function generateRandomFullName() {
+    const firstName = firstNames[ randomNumber(firstNames.length - 1) ];
+    const lastName = lastNames[ randomNumber(lastNames.length - 1) ];
+    return [ firstName, lastName ];
+}
+
+async function createMockCustomer(){
+    try {
+        const customerData = randomCustomerData();
+    
+        const numberOfAddresses = randomNumber(100) > 17 ? 1 : 2;
+        const addressesData = [] as TAddress_No_ID[];
+    
+        for (let i = 0; i < numberOfAddresses; i++) {
+            addressesData.push(randomAddress());
+        }
+        const result = await customer_create(customerData, addressesData);
+        if (result instanceof CustomerOperationError || result instanceof AddressOperationError) {
+           return await createMockCustomer();
+        }
+        return result;
+    } catch (err) {
+        if (err instanceof CustomerOperationError || err instanceof AddressOperationError) {
+           return await createMockCustomer();
+        } else {
+            console.log("unknown error happened while generating a mock customer", err);
+            throw err;
+        }
     }
-    if ('addressId' in obj) {
-        delete obj['addressId']
+}
+function randomCustomerData() {
+    const customer = {} as TCustomer_No_ID;
+    const [ first, last ] = generateRandomFullName();
+    console.log("GENERATING DATA FOR  ", first , last)
+    customer.firstName = first;
+    customer.lastName = last;
+    customer.contact = `${customer.firstName}.${customer.lastName}@example.com`;
+    customer.suspended = randomNumber(100) <= 17;
+    if (customer.suspended) {
+        const createdAt = randomDateInPast(90);
+        customer.suspendedAt = randomDateBetween(createdAt, new Date());
+    } else {
+        customer.suspendedAt = null;
     }
-    if ('customerId' in obj) {
-        delete obj['customerId']
+    customer.createdAt = randomDateInPast(90);
+    customer.updatedAt = customer.suspended
+        ? (customer.suspendedAt as Date)
+        : randomDateBetween(new Date(customer.createdAt), new Date());
+
+    return customer;
+}
+
+function randomAddress() {
+    const address = {} as TAddress_No_ID;
+
+    address.number = randomNumber(100) + "";
+    address.line1 = streetNames[ randomNumber(streetNames.length) - 1 ] + streetTypes[ randomNumber(streetTypes.length) - 1 ];
+    if (randomNumber(100) <= 17) {
+        address.line2 = addressLine2Types[ randomNumber(addressLine2Types.length) - 1 ];
+    } else {
+        address.line2 = null;
     }
-    if ('createdAt' in obj) {
-        delete obj['createdAt']
+    address.suburb = suburbs[ randomNumber(suburbs.length) - 1 ];
+    address.archived = randomNumber(10) <= 3;
+    address.createdAt = randomDateInPast(90);
+    address.updatedAt = randomDateBetween(new Date(address.createdAt), new Date());
+    return address;
+}
+
+function randomAppointment() {
+    const appointment = {} as TAppointment_No_ID;
+
+    appointment.completed = randomNumber(10) <= 7;
+    const appointmentLength = randomNumber(2);
+    const startHour = 8 + randomNumber(7);
+    const startMinutes = randomNumber(1) === 1 ? 30 : 0;
+    if (appointment.completed) {
+        const startDate = randomDateInPast(90);
+        const intermediaryDate = new Date(
+            startDate.getFullYear(),
+            startDate.getMonth(),
+            startDate.getDate(),
+            startHour,
+            startMinutes
+        )
+        appointment.start = intermediaryDate.toISOString();
+
+        const endDate = new Date(appointment.start);
+        endDate.setHours(endDate.getHours() + appointmentLength);
+        appointment.end = endDate.toISOString();
+
+        appointment.completedAt = intermediaryDate;
+    } else {
+        const startDate = randomDateInFuture(90);
+        const intermediaryDate = new Date(
+            startDate.getFullYear(),
+            startDate.getMonth(),
+            startDate.getDate(),
+            startHour,
+            startMinutes
+        )
+        appointment.start = intermediaryDate.toISOString();
+
+        const endDate = new Date(appointment.start);
+        endDate.setHours(endDate.getHours() + appointmentLength);
+        appointment.end = endDate.toISOString();
+
+        appointment.completedAt = null;
     }
-    if ('updatedAt' in obj) {
-        delete obj['updatedAt']
-    }
-    return obj
+
+    appointment.recurring = randomNumber(100) > 50;
+    appointment.frequency = appointment.recurring ? appointment.frequency = randomNumber(4) * 7 : null;
+    appointment.createdAt = randomDateInPast(90);
+    appointment.updatedAt = randomDateBetween(new Date(appointment.createdAt), new Date());
+    return appointment;
 }
--- tests/mocks/model_data.ts
+++ tests/mocks/model_data.ts
@@ -1,75 +1,69 @@
-import { TmockAddressData, TmockAppointment, TmockCustomerData } from '@types';
+import {
+    TCustomer_data_for_creation,
+    TAddress_data_for_creation,
+    TAppointment_data_for_creation,
+} from '@types';
 
-const customers: TmockCustomerData[] = [
+const customers: TCustomer_data_for_creation[] = [
     {
         firstName: 'John',
         lastName: 'Doe',
         contact: 'john.doe@test.com',
-        note: 'Note for John Doe',
     },
     {
         firstName: 'Jane',
         lastName: 'Smith',
         contact: 'jane.smith@test.com',
-        note: 'Note for Jane Smith',
     },
     {
         firstName: 'Bob',
         lastName: 'Johnson',
         contact: 'bob.johnson@test.com',
-        note: 'Note for Bob Johnson',
     },
     {
         firstName: 'Alice',
         lastName: 'Williams',
         contact: 'alice.williams@test.com',
-        note: 'Note for Alice Williams',
     },
     {
         firstName: 'Charlie',
         lastName: 'Brown',
         contact: 'charlie.brown@test.com',
-        note: 'Note for Charlie Brown',
     },
     {
         firstName: 'Eva',
         lastName: 'Davis',
         contact: 'eva.davis@test.com',
-        note: 'Note for Eva Davis',
     },
     {
         firstName: 'Frank',
         lastName: 'Miller',
         contact: 'frank.miller@test.com',
-        note: 'Note for Frank Miller',
     },
     {
         firstName: 'Grace',
         lastName: 'Wilson',
         contact: 'grace.wilson@test.com',
-        note: 'Note for Grace Wilson',
     },
     {
         firstName: 'Henry',
         lastName: 'Thomas',
         contact: 'henry.thomas@test.com',
-        note: 'Note for Henry Thomas',
     },
     {
         firstName: 'Ivy',
         lastName: 'Moore',
         contact: 'ivy.moore@test.com',
-        note: 'Note for Ivy Moore',
     },
 ];
 
-const addresses: TmockAddressData[] = [
+const addresses: TAddress_data_for_creation[] = [
     {
         number: '123',
         line1: 'Main Street',
         line2: 'Apt 4',
         suburb: 'Cityville',
-        note: 'Note for Address 123 Main Street',
+
         customerId: null,
     },
     {
@@ -77,7 +71,7 @@ const addresses: TmockAddressData[] = [
         line1: 'Oak Avenue',
         line2: '',
         suburb: 'Suburbia',
-        note: 'Note for Address 456 Oak Avenue',
+
         customerId: null,
     },
     {
@@ -85,7 +79,7 @@ const addresses: TmockAddressData[] = [
         line1: 'Pine Street',
         line2: 'Suite 10',
         suburb: 'Townsville',
-        note: 'Note for Address 789 Pine Street',
+
         customerId: null,
     },
     {
@@ -93,7 +87,7 @@ const addresses: TmockAddressData[] = [
         line1: 'Cedar Road',
         line2: '',
         suburb: 'Villageland',
-        note: 'Note for Address 1011 Cedar Road',
+
         customerId: null,
     },
     {
@@ -101,7 +95,7 @@ const addresses: TmockAddressData[] = [
         line1: 'Maple Lane',
         line2: 'Unit 7',
         suburb: 'Hamlet City',
-        note: 'Note for Address 1213 Maple Lane',
+
         customerId: null,
     },
     {
@@ -109,7 +103,7 @@ const addresses: TmockAddressData[] = [
         line1: 'Birch Avenue',
         line2: '',
         suburb: 'Countryside',
-        note: 'Note for Address 1415 Birch Avenue',
+
         customerId: null,
     },
     {
@@ -117,7 +111,7 @@ const addresses: TmockAddressData[] = [
         line1: 'Spruce Street',
         line2: 'Apt 3',
         suburb: 'Meadowland',
-        note: 'Note for Address 1617 Spruce Street',
+
         customerId: null,
     },
     {
@@ -125,7 +119,7 @@ const addresses: TmockAddressData[] = [
         line1: 'Redwood Road',
         line2: '',
         suburb: 'Hometown',
-        note: 'Note for Address 1819 Redwood Road',
+
         customerId: null,
     },
     {
@@ -133,7 +127,7 @@ const addresses: TmockAddressData[] = [
         line1: 'Fir Lane',
         line2: 'Suite 5',
         suburb: 'Greenfield',
-        note: 'Note for Address 2021 Fir Lane',
+
         customerId: null,
     },
     {
@@ -141,53 +135,47 @@ const addresses: TmockAddressData[] = [
         line1: 'Holly Street',
         line2: '',
         suburb: 'Cottageville',
-        note: 'Note for Address 2223 Holly Street',
+
         customerId: null,
     },
 ];
 
-const appointments: TmockAppointment[] = [
+const appointments: TAppointment_data_for_creation[] = [
     {
         recurring: false,
         frequency: null,
         start: '2023-10-31T10:00:00Z',
         end: '2023-10-31T11:00:00Z',
-        note: 'Note for Appointment on 2023-10-31',
     },
     {
         recurring: true,
         frequency: 7,
         start: '2023-11-01T14:00:00Z',
         end: '2023-11-01T15:00:00Z',
-        note: 'Note for Recurring Appointment on 2023-11-01',
     },
     {
         recurring: false,
         frequency: null,
         start: '2023-11-02T09:00:00Z',
         end: '2023-11-02T10:00:00Z',
-        note: 'Note for Appointment on 2023-11-02',
     },
     {
         recurring: false,
         frequency: null,
         start: '2023-11-03T15:30:00Z',
         end: '2023-11-03T16:30:00Z',
-        note: 'Note for Appointment on 2023-11-03',
     },
     {
         recurring: false,
         frequency: null,
         start: '2023-11-04T12:00:00Z',
         end: '2023-11-04T13:00:00Z',
-        note: 'Note for Appointment on 2023-11-04',
     },
     {
         recurring: false,
         frequency: null,
         start: '2023-11-05T11:00:00Z',
         end: '2023-11-05T12:00:00Z',
-        note: 'Note for Appointment on 2023-11-05',
     },
 ];
 
