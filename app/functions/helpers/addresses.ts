8e0848f31ed2d893fdd0c5b4f6ff6b708fcd3477
carl davidson <carl.davidson@hotmail.com>
2023-12-23 10:14:39 +1000

    updated addFullAddress to handle address and appointment types

--- app/functions/helpers/addresses.ts
+++ app/functions/helpers/addresses.ts
@@ -1,30 +1,8 @@
 import {
-    TCustomer,
     TAppointmentWithCustomerNameAndFullAddress,
     TAddressWithCustomerNameAndFullAddress,
 } from '@types';
-import { isAddress, isAppointment, isCustomer } from '@functions';
-
-// export function getAddressesFromCustomerArray(customers: TCustomer[] = []) {
-//   console.log(" getAddressesFromCustomerArray START")
-//   const addresses = customers.reduce((addressesArr, customer) => {
-//     if (customer.addresses && customer.addresses.length) {
-//       const fullName = addFullName(customer);
-
-//       addressesArr.push(
-//         ...customer.addresses.map((address) => {
-//           const addressData = addFullAddress(address);
-
-//           return Object.assign(addressData, fullName)
-//         })
-//       );
-//     }
-//     return addressesArr;
-//   }, [] as TAddressWithCustomerNameAndFullAddress[]);
-//   console.log(" getAddressesFromCustomerArray END \n number of addresses ", addresses.length );
-
-//   return addresses;
-// }
+import { isAddress, isAppointment } from '@functions';
 
 export function addFullAddress(obj: TAppointmentWithCustomerNameAndFullAddress | TAddressWithCustomerNameAndFullAddress) {
 
@@ -32,23 +10,23 @@ export function addFullAddress(obj: TAppointmentWithCustomerNameAndFullAddress |
         const { address } = obj;
         if (address && isAddress(address)) {
             const addressLine2 = address.line2 ? ` ${address.line2}` : '';
-            return {
+            const updatedAppointment: TAppointmentWithCustomerNameAndFullAddress = {
                 ...obj,
                 fullAddress: `${address.number} ${address.line1}${addressLine2}, ${address.suburb}`,
-            };
+            } ;
+            return updatedAppointment;
         }
     }
     if (isAddress(obj)){
         const addressLine2 = obj.line2 ? ` ${obj.line2}` : '';
-        return {
+        const updatedAddress: TAddressWithCustomerNameAndFullAddress = {
             ...obj,
             fullAddress: `${obj.number} ${obj.line1}${addressLine2}, ${obj.suburb}`,
         };
+        return updatedAddress;
     }
 
-    return obj && typeof obj === 'object'
-    ? { ...(obj as Record<string, unknown>), fullAddress: null }
-    : null;
+    return obj;
 }
 
 
