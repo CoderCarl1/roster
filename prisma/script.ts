9cb36a17d43e1cb9b589fd074df890fbbcd9cdda
carl davidson <carl.davidson@hotmail.com>
2023-12-28 10:02:16 +1000

    move seeding data into its own file

--- prisma/script.ts
+++ prisma/script.ts
@@ -1,11 +1,8 @@
 import { PrismaClient } from '@prisma/client'
-import { AddressOperationError, AppointmentOperationError, CustomerOperationError } from '@errors';
-import { TAddress_No_ID, TAppointment_No_ID, TCustomer, TCustomer_No_ID } from '@types'
 import { log } from '~/functions/helpers/functions';
-import { appointment_create } from '~/models/appointment.server';
-import { customer_create, customer_delete_many } from '~/models/customer.server';
+import { customer_delete_many } from '~/models/customer.server';
 import { singleton } from '~/singleton.server';
-import { firstNames, lastNames, addressLine2Types, streetNames, streetTypes, suburbs } from '../app/lib/placeholder-data';
+import { createAppointments, createCustomers } from '~/lib';
 
 const seedingFlagIndex = process.argv.indexOf('--seeding') + 1
 if (seedingFlagIndex !== -1) {
@@ -20,8 +17,6 @@ const prisma = singleton(
         }),
     process.env.SEEDING
 )
-
-
 
 try {
     main();
@@ -74,195 +69,8 @@ async function seed(numberOfCustomersToCreate = 30) {
     }
 }
 
-async function createCustomers(numberOfCustomersToCreate: number){
-    const customers: Pick<TCustomer, 'id' | 'addresses'>[] = [];
-
-    for (let i = 0; i < numberOfCustomersToCreate; i++) {
-        const result: TCustomer = await createMockCustomer();
-        const { id, addresses } = result;
-        customers.push({ id, addresses });
-    }
-    return Promise.resolve(customers);
-}
-
-async function createAppointments(customers: Pick<TCustomer, 'id' | 'addresses'>[]) {
-    const appointmentsPromises = customers.flatMap(async ({ id, addresses }) => {
-        const nestedAppointmentsPromises = [];
-
-        if (addresses) {
-            for (const address of addresses) {
-                const appointmentData = randomAppointment();
-                const appointment = await appointment_create(id, address.id, appointmentData);
-
-                if (appointment && !(appointment instanceof AppointmentOperationError) && appointment.frequency) {
-                    const appointmentsToCreate = appointmentsPerYear(appointment.frequency) - 1;
-                    const nestedAppointmentData = Array.from({ length: appointmentsToCreate }, () => ({
-                        ...randomAppointment(),
-                        frequency: appointment.frequency,
-                    }));
-
-                    const nestedPromises = nestedAppointmentData.map((nestedAppointment) =>
-                        appointment_create(id, address.id, nestedAppointment)
-                    );
-
-                    nestedAppointmentsPromises.push(...nestedPromises);
-                }
-            }
-        }
-
-        return nestedAppointmentsPromises;
-    });
-
-    return await Promise.all(appointmentsPromises);
-    
-}
-
-// function addWeeks(date: Date, weeks: number) {
-//     const dateCopy = new Date(date);
-//     dateCopy.setDate(date.getDate() + 7 * weeks);
-//     return dateCopy.toISOString();
-// }
-function appointmentsPerYear(appointmentFrequency: number) {
-    const daysInYear = 365;
-    return Math.floor(daysInYear / appointmentFrequency);
-}
-
-function randomNumber(ceiling: number) {
-    return Math.floor(Math.random() * ceiling) + 1;
-}
-
-function randomDateInPast(days: number) {
-    const pastDate = new Date();
-    pastDate.setDate(pastDate.getDate() - randomNumber(days));
-    return pastDate;
-}
-
-function randomDateInFuture(days: number) {
-    const futureDate = new Date();
-    futureDate.setDate(futureDate.getDate() + randomNumber(days));
-    return futureDate;
-}
-
-function randomDateBetween(startDate: Date, endDate: Date) {
-    const startTimestamp = startDate.getTime();
-    const endTimestamp = endDate.getTime();
-    const randomTimestamp = startTimestamp + Math.random() * (endTimestamp - startTimestamp);
-    return new Date(randomTimestamp);
-}
 
 
-function generateRandomFullName() {
-    const firstName = firstNames[ randomNumber(firstNames.length - 1) ];
-    const lastName = lastNames[ randomNumber(lastNames.length - 1) ];
-    return [ firstName, lastName ];
-}
 
-async function createMockCustomer(retriesLeft = 1) {
-    try {
-        const customerData = randomCustomerData();
-        const numberOfAddresses = randomNumber(100) > 17 ? 1 : 2;
-        const addressesData = Array.from({ length: numberOfAddresses }, randomAddress);
 
-        const result = await customer_create(customerData, addressesData);
 
-        // There is a tiny chance that there will be a duplicate customer name so retry it once
-        if (result instanceof CustomerOperationError || result instanceof AddressOperationError ) {
-            if (retriesLeft > 0){
-                return await createMockCustomer(retriesLeft - 1);
-            } else {
-                throw result;
-            }
-        }
-        
-        return result;
-    } catch (err) {
-        log({color: 'red'}, "unknown error happened while generating a mock customer", {errorData: err});
-        throw err;    
-    }
-}
-function randomCustomerData() {
-    const customer = {} as TCustomer_No_ID;
-    const [ first, last ] = generateRandomFullName();
-    customer.firstName = first;
-    customer.lastName = last;
-    customer.contact = `${customer.firstName}.${customer.lastName}@example.com`;
-    customer.suspended = randomNumber(100) <= 17;
-    if (customer.suspended) {
-        const createdAt = randomDateInPast(90);
-        customer.suspendedAt = randomDateBetween(createdAt, new Date());
-    } else {
-        customer.suspendedAt = null;
-    }
-    customer.createdAt = randomDateInPast(90);
-    customer.updatedAt = customer.suspended
-        ? customer.suspendedAt
-        : randomDateBetween(new Date(customer.createdAt), new Date());
-
-    return customer;
-}
-
-function randomAddress() {
-    const address = {} as TAddress_No_ID;
-
-    address.number = randomNumber(100) + "";
-    address.line1 = streetNames[ randomNumber(streetNames.length) - 1 ] + " " + streetTypes[ randomNumber(streetTypes.length) - 1 ];
-    if (randomNumber(100) <= 17) {
-        address.line2 = addressLine2Types[ randomNumber(addressLine2Types.length) - 1 ] + " example";
-    } else {
-        address.line2 = "example";
-    }
-    address.suburb = suburbs[ randomNumber(suburbs.length) - 1 ];
-    address.archived = randomNumber(10) <= 3;
-    address.createdAt = randomDateInPast(90);
-    address.updatedAt = randomDateBetween(new Date(address.createdAt), new Date());
-    return address;
-}
-
-function randomAppointment() {
-    const appointment = {} as TAppointment_No_ID;
-
-    appointment.completed = randomNumber(10) <= 7;
-    const appointmentLength = randomNumber(2);
-    // start time of appointments betwen 8:00 and 15:00
-    const startHour = 8 + randomNumber(7);
-    const startMinutes = (randomNumber(4) * 15 ) - 15;
-    if (appointment.completed) {
-        const startDate = randomDateInPast(90);
-        const intermediaryDate = new Date(
-            startDate.getFullYear(),
-            startDate.getMonth(),
-            startDate.getDate(),
-            startHour,
-            startMinutes
-        )
-        appointment.start = intermediaryDate.toISOString();
-
-        const endDate = new Date(appointment.start);
-        endDate.setHours(endDate.getHours() + appointmentLength);
-        appointment.end = endDate.toISOString();
-
-        appointment.completedAt = intermediaryDate;
-    } else {
-        const startDate = randomDateInFuture(90);
-        const intermediaryDate = new Date(
-            startDate.getFullYear(),
-            startDate.getMonth(),
-            startDate.getDate(),
-            startHour,
-            startMinutes
-        )
-        appointment.start = intermediaryDate.toISOString();
-
-        const endDate = new Date(appointment.start);
-        endDate.setHours(endDate.getHours() + appointmentLength);
-        appointment.end = endDate.toISOString();
-
-        appointment.completedAt = null;
-    }
-
-    appointment.recurring = randomNumber(100) > 50;
-    appointment.frequency = appointment.recurring ? appointment.frequency = randomNumber(4) * 7 : null;
-    appointment.createdAt = randomDateInPast(90);
-    appointment.updatedAt = randomDateBetween(new Date(appointment.createdAt), new Date());
-    return appointment;
-}
