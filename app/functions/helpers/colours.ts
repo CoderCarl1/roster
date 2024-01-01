b7adf42caf5458c498d509bd093d6b69cc182ffc
carl davidson <carl.davidson@hotmail.com>
2024-01-01 14:23:39 +1000

    add colour funcs into own file

---
+++ app/functions/helpers/colours.ts
@@ -0,0 +1,11 @@
+import { randomNumber } from "..";
+
+
+export function randomHSLValues(){
+  const hue = randomNumber(12) * 30;
+  const saturation = Math.max(70, randomNumber(100));
+  const lightness =  randomNumber(70) + 15;
+
+  return {hue, saturation, lightness};
+
+}
