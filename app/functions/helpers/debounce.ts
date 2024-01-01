656874637a90020ea887d25642828162a2ace4a4
carl davidson <carl.davidson@hotmail.com>
2024-01-01 14:19:57 +1000

    move debounce into own file

---
+++ app/functions/helpers/debounce.ts
@@ -0,0 +1,29 @@
+/**
+ * Debounces a function to be called after a specified delay.
+ *
+ * @param func The function to debounce.
+ * @param delay The delay (in milliseconds) before the debounced function is called.
+ * @param args The arguments to pass to the debounced function.
+ * @example
+ * debounce(searchAddress, 300, searchString);
+ */
+
+type debounceProps<T> = {
+  func: (args: T) => void;
+  delay: number;
+  args: T;
+};
+function debounce<T>({ func, delay, args }: debounceProps<T>): void {
+  let debounceTimer: NodeJS.Timeout | null = null;
+
+  function setTimer() {
+      if (debounceTimer) {
+          clearTimeout(debounceTimer);
+      }
+      debounceTimer = setTimeout(() => {
+          func(args);
+      }, delay);
+  }
+
+  setTimer();
+}
