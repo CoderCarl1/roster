550d5717e8d2aa9e3d1b2f5edc71a09c140bcf51
carl davidson <carl.davidson@hotmail.com>
2024-01-03 13:46:19 +1000

    add useClickOutside hook

---
+++ app/functions/helpers/useClickOutside.ts
@@ -0,0 +1,45 @@
+import { useEffect, useRef } from "react";
+
+type clickOutsideProps<T> = {
+  cb: (value?: MouseEvent) => unknown;
+  initialRef?: React.RefObject<T>;
+  checkBeforeRunningCB?: Boolean[];
+}
+/**
+ * Monitors clicks outside of a specified DOM element and triggers the passed in callback when a click occurs outside of it.
+ *
+ * @param cb - The callback function to invoke when a click occurs outside the DOM element.
+ * @param initialRef - The reference to the DOM element that you want to monitor for outside clicks.
+ *
+ * @returns A reference to the same DOM element.
+ */
+export function UseClickOutside<T extends HTMLElement>(
+  { cb, initialRef, checkBeforeRunningCB }: clickOutsideProps<T>
+): React.MutableRefObject<T | null> {
+  const domRef = useRef(initialRef ? initialRef.current : null);
+
+  useEffect(() => {
+    function outsideClickHandler(event: MouseEvent) {
+      if (checkBeforeRunningCB && !checkBeforeRunningCB.every(Boolean)) {
+        return;
+      }
+      const target = event.target as HTMLElement;
+
+      if (domRef.current && !domRef.current?.contains(target)) {
+        cb();
+        return;
+      }
+    }
+
+    document.addEventListener("mousedown", outsideClickHandler, true);
+
+    return () => {
+      document.removeEventListener("mousedown", outsideClickHandler, true);
+    };
+  }, [ domRef, cb ]);
+
+
+  return domRef;
+}
+
+export default UseClickOutside;
